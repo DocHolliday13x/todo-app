4b3a7d586674a97931547414944290a2ca2e3a86
Ryan Eastman <rking1323@gmail.com>
2023-06-27 16:36:17 -0700

    added basic header/footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,10 @@
+const Footer = (props) => {
+  return (
+    <footer>
+      <p>© 2023 Code Fellows</p>
+    </footer>
+  );
+}
+
+
+export default Footer;
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+const Header = (props) => {
+  return (
+    <header>
+      <h1>My Todo List</h1>
+    </header>
+  );
+}
+
+
+export default Header;
+
+
+
+
