2c222151a58b029065c60160302de1b0e6dde5b9
Ryan Eastman <rking1323@gmail.com>
2023-06-28 23:29:14 -0700

    debugging

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import SettingsProvider from './Context/Settings';
-import ModeProvider from './Context/Mode';
 import { MantineProvider } from '@mantine/core';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
