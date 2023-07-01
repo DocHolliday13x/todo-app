a05412528fa164f96d759fa2df612f70e4b92d1a
Ryan Eastman <rking1323@gmail.com>
2023-06-30 23:22:32 -0700

    heroku is being an asshole, i'm done

--- src/Context/Auth/index.jsx
+++ src/Context/Auth/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import users from './lib/users.js';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+// import users from './lib/users.js';
 import jwt_encode from 'jwt-encode';
+import cookie from 'react-cookies';
 
 export const AuthContext = React.createContext();
 
@@ -8,32 +10,63 @@ function AuthProvider({ children }) {
 
   const [user, setUser] = React.useState({});
   const [token, setToken] = React.useState('');
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-  const login = (username, password) => {
-    const user = users.find(user => user.username === username);
-    if (user && user.password === password) {
-      const token = jwt_encode({ username: user.username });
-      setToken(token);
+  useEffect(() => {
+    const token = cookie.load('auth');
+    validateToken(token);
+  }, []);
+
+  const validateToken = (token) => {
+    try {
+      const user = jwt_encode(token);
       setUser(user);
+      setToken(token);
+      setIsLoggedIn(true);
+    } catch (e) {
+      setUser({});
+      setToken('');
+      setIsLoggedIn(false);
     }
   }
 
+  const login = async (username, password) => {
+    const config = {
+      method: 'post',
+      baseURL: 'https://api-401js.herokuapp.com',
+      url: '/signin',
+      method: 'post',
+      auth: {
+        username: username,
+        password: password
+      }
+    }
+    const response = await axios(config);
+    console.log('RESPONSE FROM AXIOS -----------', response.data);
+    const { token } = response.data;
+    validateToken(token);
+  }
+
   const logout = () => {
-    setToken('');
     setUser({});
+    setToken('');
+    setIsLoggedIn(false);
   }
 
   const can = (capability) => {
-    return user?.capabilities?.includes(capability);
+    return user.capabilities && user.capabilities.includes(capability);
   }
 
   const state = {
     user,
     token,
+    isLoggedIn,
     login,
     logout,
-    can,
+    can
   }
+
+
 
   return (
     <AuthContext.Provider value={state}>
